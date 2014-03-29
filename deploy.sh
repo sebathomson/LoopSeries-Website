f377f56f7f4c4bcec6c34353ada091c8798150b5
Josh <josluis.lopes@gmail.com>
2014-03-29 18:40:52 +0000

    Adding some configs so FOS work.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 php composer.phar update
+php app/console cache:clear --env=prod
+php app/console cache:clear --env=dev
 php app/console assetic:dump
 php app/console assets:install --symlink
 php app/console doctrine:schema:update --force
