1d43cefc4d3bbbf4e0cdaa4ae70345f185083ad7
Jose Lopes <jose@shopcade.com>
2014-05-16 20:24:40 +0100

    removed localhost on config, add return folders on appKernel for be faster

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -39,4 +39,14 @@ class AppKernel extends Kernel
     {
         $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
     }
+
+    public function getCacheDir()
+    {
+        return '/tmp/symfony/cache/'. $this->environment;
+    }
+
+    public function getLogDir()
+    {
+        return '/tmp/symfony/log/'. $this->environment;
+    }
 }
