f2d346aa631289394891da7f0af5301b68750f7b
Jose Lopes <jose@shopcade.com>
2014-08-11 23:14:17 +0100

    a

--- src/LoopAnime/ShowsBundle/Services/VideoService.php
+++ src/LoopAnime/ShowsBundle/Services/VideoService.php
@@ -21,6 +21,7 @@ class VideoService
         // TODO i dont know why i cant use a factory like this should be like this!
         //$hoster = new $hoster();
         $hoster = explode("-",$hoster)[0]; // TODO this should be only temporary -- All old links hoster should be updated to not have the hyphen and 2nd level hoster
+
         switch($hoster) {
             case "Anime44":
                 $hoster = new Anime44();
