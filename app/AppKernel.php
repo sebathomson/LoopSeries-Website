fbf38243402502a94d5c4fbb8daca438d435c29c
mferreira <mferreira@rocket-internet.pt>
2014-09-08 11:26:37 +0100

    re-enable fos auth server bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -29,7 +29,7 @@ class AppKernel extends Kernel
             new LoopAnime\AdminBundle\LoopAnimeAdminBundle(),
             new LoopAnime\SearchBundle\LoopAnimeSearchBundle(),
             new Liip\ImagineBundle\LiipImagineBundle(),
-            //new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
+            new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
